609309a4f252d34a514639cd7dd87c101fdbee6b
Arafat <arafathossain.cs@gmail.com>
2025-04-27 15:46:23 +0600

    first commit

---
+++ factorial.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num)
+{
+
+    if (num == 1)
+        return 1;
+
+    else
+        return num * factorial(num - 1);
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter the number: ";
+    cin >> number;
+
+    if (number <= 0)
+    {
+        cout << "The input number is invalid.";
+    }
+
+    int result = factorial(number);
+
+    cout << "The factorial of " << number << "th is: " << result;
+    return 0;
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int number)
+{
+    if (number == 0)
+        return 0;
+    if (number == 1)
+        return 1;
+
+    else
+        return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Give the number to find fibonacci: ";
+    cin >> n;
+
+    int result = fibonacci(n);
+    cout << "The fibonacci of " << n << "th number" << " is: " << result;
+    return 0;
+}
