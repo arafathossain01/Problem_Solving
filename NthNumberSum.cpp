76bdce6afbda618c960a76ace24ab60084d0c53a
Arafat <arafathossain.cs@gmail.com>
2025-05-31 20:30:39 +0600

    Nth Number

---
+++ NthNumberSum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int NthNum(int n)
+{
+    if (n == 0)
+        return 0;
+
+    else
+    {
+        int temp = n + NthNum(n - 1);
+        return temp;
+    }
+}
+
+int main()
+{
+
+    int num;
+    cout << "Enter the Nth number: ";
+    cin >> num;
+
+    int result = NthNum(num);
+    cout << "The sum of first " << num << " number is: " << result;
+
+    return 0;
+}
